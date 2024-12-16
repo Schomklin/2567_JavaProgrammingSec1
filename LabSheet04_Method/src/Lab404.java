28eddf69f25e6d44381469aa241a82b55f2f700d
Schomklin <kungchomklin@gmail.com>
2024-12-16 13:59:39 +0700

    Lab404

--- LabSheet04_Method/src/Lab404.java
+++ LabSheet04_Method/src/Lab404.java
@@ -1,9 +1,59 @@
+import java.util.*;
 
 public class Lab404 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        inputStudent();
+    }
 
-	}
+    public static void inputStudent() {
+        Scanner scanner = new Scanner(System.in);
+        String studentID, subjectCode;
+        boolean isStudentIDValid, isSubjectCodeValid;
+        
+        while (true) {
+            // Input student ID
+            System.out.print("Enter student ID (10 digits): ");
+            studentID = scanner.nextLine();
+
+            // Input subject code
+            System.out.print("Enter subject code (7 digits): ");
+            subjectCode = scanner.nextLine();
+
+            // Check lengths of student ID and subject code
+            isStudentIDValid = isLength(studentID, 10);
+            isSubjectCodeValid = isLength(subjectCode, 7);
+            
+            if(isStudentIDValid && isSubjectCodeValid) {
+            	//Check student details
+            	boolean isITStudent = isITStudent(studentID);
+            	boolean isITSubject = isITSubject(subjectCode);
+            	
+            	//Display data
+            	displayData(isITStudent, isITSubject);
+            	break;            	     	
+            }else {
+            	System.out.println("Invalid input! Student ID must be 10 digits and "
+            			+ "Subject Code must be 7 digits");
+            }
+            scanner.close();              
+        }
+    }
+    
+    public static boolean isLength(String input, int requiredLength) {
+            
+    }
+    
+    public static boolean isITStudent(String studentID) {
+         
+    }
+    
+    public static boolean isITSubject(String subjectCode) {
+     
+    }
+    
+    public static void displayData(boolean isITStudent, boolean isITSubject) {
+
+    }
 
 }
